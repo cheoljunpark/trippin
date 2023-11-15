06e9702e734c91a02351f3be686e37487f51cb9a
aruma <dkfnak6789@gmail.com>
2023-11-15 17:17:21 +0900

    feat: add UserLogin domain
    UserLogin 도메인 추가

---
+++ src/main/java/com/trippin/api/user/domain/UserLogin.java
@@ -0,0 +1,36 @@
+package com.trippin.api.user.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Table(name = "user_login")
+public class UserLogin {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "email", nullable = false)
+  private String email;
+
+  @Column(name = "username", nullable = false)
+  private String userName;
+
+  @Column(name = "token")
+  private String token;
+
+
+}
