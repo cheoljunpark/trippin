dbf67e7518e927ef4d94c094c877100d119dd49e
aruma <dkfnak6789@gmail.com>
2023-11-19 13:46:25 +0900

    chore: add swagger info

--- src/main/java/com/trippin/api/user/dto/JoinDto.java
+++ src/main/java/com/trippin/api/user/dto/JoinDto.java
@@ -2,26 +2,33 @@ package com.trippin.api.user.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.trippin.api.user.domain.UserLogin;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@ApiModel(value = "회원가입")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class JoinDto {
 
+  @ApiModelProperty(value = "유저 이메일", example = "ssafy@gmail.com", required = true)
   @JsonProperty("email")
   private String email;
 
+  @ApiModelProperty(value = "유저 네임", example = "ssafy", required = true)
   @JsonProperty("username")
   private String userName;
 
+  @ApiModelProperty(value = "비밀번호", example = "1234", required = true)
   @JsonProperty("password")
   private String password;
 
+  @ApiModelProperty(value = "비밀번호 확인", example = "1234", required = true)
   @JsonProperty("password-confirm")
   private String passwordConfirm;
 
