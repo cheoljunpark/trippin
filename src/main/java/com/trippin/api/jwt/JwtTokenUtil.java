be32289cc6b13c5569ef1aaa9eee05e159cdabe7
aruma <dkfnak6789@gmail.com>
2023-11-20 14:18:04 +0900

    fix: put claims loginId to userLogin

--- src/main/java/com/trippin/api/jwt/JwtTokenUtil.java
+++ src/main/java/com/trippin/api/jwt/JwtTokenUtil.java
@@ -1,5 +1,6 @@
 package com.trippin.api.jwt;
 
+import com.trippin.api.user.domain.UserLogin;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -8,11 +9,11 @@ import java.util.Date;
 public class JwtTokenUtil {
 
   // JWT Token 발급
-  public static String createToken(String loginId, String key, long expireTimeMs) {
+  public static String createToken(UserLogin userLogin, String key, long expireTimeMs) {
     // Claim = Jwt Token에 들어갈 정보
     // Claim에 loginId를 넣어 줌으로써 나중에 loginId를 꺼낼 수 있음
     Claims claims = Jwts.claims();
-    claims.put("loginId", loginId);
+    claims.put("userLogin", userLogin);
 
     return Jwts.builder()
         .setClaims(claims)
@@ -27,7 +28,7 @@ public class JwtTokenUtil {
     return extractClaims(token, secretKey).get("loginId").toString();
   }
 
-  // 밝급된 Token이 만료 시간이 지났는지 체크
+  // 발급된 Token이 만료 시간이 지났는지 체크
   public static boolean isExpired(String token, String secretKey) {
     Date expiredDate = extractClaims(token, secretKey).getExpiration();
     // Token의 만료 날짜가 지금보다 이전인지 check
